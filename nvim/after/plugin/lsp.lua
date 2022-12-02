79ecb389fc400d61d7b761b71a4d144de8f1838a
Praveen Kumar Koram <pkkoram98@gmail.com>
2022-12-02 15:30:07 +0530

    update lsp function name

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -84,7 +84,7 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local options = {
   on_attach = on_attach,
