7ed7261ec3fb12345c114561945fa1dc9ed1b80a
Praveen Kumar Koram <pkkoram98@gmail.com>
2023-07-08 00:21:22 +0530

    properly setup tokyonight

--- nvim/lua/p/plugins/colorscheme.lua
+++ nvim/lua/p/plugins/colorscheme.lua
@@ -5,9 +5,18 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
       style = 'night',
+      transparent = true,
+      on_highlights = function(hl, c)
+        hl.CursorLineNr = {
+          bold = true,
+          bg = c.bg_dark,
+          fg = c.orange,
+        }
+      end,
     },
-    config = function()
-      vim.cmd [[colorscheme tokyonight-night]]
+    config = function(_, opts)
+      require('tokyonight').setup(opts)
+      require('tokyonight').load()
     end,
   },
 }
